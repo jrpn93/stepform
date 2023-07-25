1aab74b01887369a0c3a9e41d4e675372aa43e6a
jrpn93 <jrpn93@gmail.com>
2023-07-25 23:05:01 +0530

    kuch to...

---
+++ src/Assets/Constant/index.js
@@ -0,0 +1,13 @@
+export const Images = {
+  SidebarBG: '../../Assets/Images/bgsidebarmobile.png',
+};
+export const Fonts = {
+  Ubuntu_Bold: 'Ubuntu-Bold',
+  Ubuntu_BoldItalic: 'Ubuntu-BoldItalic',
+  Ubuntu_Italic: 'Ubuntu-BoldItalic',
+  Ubuntu_Light: 'Ubuntu-Light',
+  Ubuntu_LightItalic: 'Ubuntu-LightItalic',
+  Ubuntu_Medium: 'Ubuntu-Medium',
+  Ubuntu_MediumItalic: 'Ubuntu-MediumItalic',
+  Ubuntu_MediumItalic: 'Ubuntu-MediumItalic',
+};
--- src/Route/index.js
+++ src/Route/index.js
@@ -1,15 +1,74 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import tw from 'twrnc';
 import '../../tailwind.config';
+import FastImage from 'react-native-fast-image';
+import {Fonts, Images} from '../Assets/Constant';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from '../../Utils/LayoutMesurement';
+import helo from '../Assets/Images/bgsidebarmobile.png';
 
 export default My = () => {
   return (
-    <View style={tw`flex flex-1 p-4 android:pt-2 bg-black dark:bg-black`}>
-      <Text style={[{fontFamily:'Ubuntu-BoldItalic'},tw`text-white`]}>
-        Hello world {'\n'}Welcome to Tailwind Css template in React Native
-      </Text>
-      <View></View>
+    <View style={tw`flex flex-1 bg-sky-100`}>
+      <View style={tw`h-[150px]`}>
+        <FastImage
+          source={helo}
+          style={tw`w-full h-full`}
+          resizeMode="contain"
+        />
+      </View>
+      <View
+        style={tw`absolute self-center mt-4 flex flex-row justify-center gap-3 `}>
+        <View
+          style={tw`flex bg-blue-400 items-center h-9 w-9 rounded-full border border-white`}>
+          <Text
+            style={[
+              tw`flex flex-1 font-bold text-xl text-black`,
+              {textAlignVertical: 'center'},
+            ]}>
+            1
+          </Text>
+        </View>
+        <View
+          style={tw`flex items-center h-9 w-9 rounded-full border border-white `}>
+          <Text
+            style={[
+              tw`flex flex-1 font-bold text-xl `,
+              {textAlignVertical: 'center'},
+            ]}>
+            2
+          </Text>
+        </View>
+        <View
+          style={tw`flex items-center h-9 w-9 rounded-full border border-white`}>
+          <Text
+            style={[
+              tw`flex flex-1 font-bold text-xl`,
+              {textAlignVertical: 'center'},
+            ]}>
+            3
+          </Text>
+        </View>
+        <View
+          style={tw`flex items-center h-9 w-9 rounded-full border border-white`}>
+          <Text
+            style={[
+              tw`flex flex-1 font-bold text-xl`,
+              {textAlignVertical: 'center'},
+            ]}>
+            4
+          </Text>
+        </View>
+      </View>
+      <View
+        style={tw`absolute my-22 mx-4.1 w-90 h-120 px-4 py-5 rounded-xl shadow-xl bg-white`}>
+        <View>
+          <Text style={[{fontFamily:Fonts.Ubuntu_Bold},tw`text-black text-7 `]}>Personal info</Text>
+        </View>
+      </View>
     </View>
   );
 };
